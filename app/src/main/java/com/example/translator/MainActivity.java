b67b8cab1ddb0b20a1376cd1c00ce39c7e1934b7
kakathy <kakathy@gmail.com>
2022-08-28 18:54:44 +0800

    左右互换

--- app/src/main/java/com/example/translator/MainActivity.java
+++ app/src/main/java/com/example/translator/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int test=22,a=10;
+        int test=22,d=10;
 
     }
 }
